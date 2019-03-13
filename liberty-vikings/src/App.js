656930bb2104d44c5e82559a3cbc9638513cd008
David Downie <daviddownie@gmail.com>
2019-03-12 20:57:33 -0400

    set up react-router-dom and set up front page. Built initial questionairre component

--- liberty-vikings/src/App.js
+++ liberty-vikings/src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Survey from "./Survey";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand">Housing Search</a>
+          <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+          </form>
+        </nav>
+
+        {/* change to carousel of available apartments? */}
+        <div class="jumbotron">
+          <h1 class="display-4">Hello, world!</h1>
+          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr class="my-4" />
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+
+        {/* MAIN CONTENT */}
+        <Router>
+          <Switch>
+            <Route path="/survey" component={Survey} />
+            {/* <Route path="/homeowners" component={Homeowners} />
+            <Route path="/browsehousing" component={Browse} /> */}
+            {/* <Route component={ NoResults } /> */}
+          </Switch>
+        </Router>
       </div>
     );
   }
