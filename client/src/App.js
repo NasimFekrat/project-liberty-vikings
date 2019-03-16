f9d76690867af9af845e1a97673b8bba4c8da228
David Downie <daviddownie@gmail.com>
2019-03-16 13:24:44 -0400

    restructured file layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,27 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import Survey from "./components/Survey";
-
+// import Survey from "./components/Survey";
+import Home from "./pages/Home";
+// import Navbar from './components/Navbar';
+// import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
-          <a className="navbar-brand">Housing Search</a>
-          <form className="form-inline">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-          </form>
-        </nav>
-
-        {/* change to carousel of available apartments? */}
-        <div class="jumbotron">
-          <h1 class="display-4">Hello, world!</h1>
-          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr class="my-4" />
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </div>
-
-        {/* MAIN CONTENT */}
         <Router>
+          <div>
+            {/* <Navbar /> */}
           <Switch>
-            <Route path="/survey" component={Survey} />
+            {/* <Route exact path="/" component={Survey} /> */}
+            <Route exact path="/survey" component={Home} />
             {/* <Route path="/homeowners" component={Homeowners} />
             <Route path="/browsehousing" component={Browse} /> */}
             {/* <Route component={ NoResults } /> */}
           </Switch>
+          {/* <Footer /> */}
+          </div>
         </Router>
-      </div>
     );
   }
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+
+function Home() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Footer />
+
+        </div>
+    );
+}
+
+export default Home;
